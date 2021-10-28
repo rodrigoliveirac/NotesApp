a43a2dfadb75b2a0f3f319cd787722f219f7649e
Rodrigo de Oliveira Cavalcante <rodrigoliveiracavalcante@gmail.com>
2021-10-28 15:50:11 -0300

    navigation

--- app/src/main/java/com/rodrigoc/noteapp/feature_note/data/data_source/NoteDao.kt
+++ app/src/main/java/com/rodrigoc/noteapp/feature_note/data/data_source/NoteDao.kt
@@ -1,18 +1,16 @@
 package com.rodrigoc.noteapp.feature_note.data.data_source
 
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.rodrigoc.noteapp.feature_note.domain.model.Note
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface NoteDao {
     @Query("SELECT * FROM note")
     fun getNotes(): Flow<List<Note>>
 
     @Query("SELECT * FROM note WHERE id = :id")
-    suspend fun getNoteById(id: Note): Note?
+    suspend fun getNoteById(id: Int): Note?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNote(note: Note)

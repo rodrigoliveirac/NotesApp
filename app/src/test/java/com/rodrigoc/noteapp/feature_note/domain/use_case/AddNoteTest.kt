5be6df2298cd892eb8a9a804d4e308609d7fdbff
Rodrigo de Oliveira Cavalcante <rodrigoliveiracavalcante@gmail.com>
2021-11-01 09:05:57 -0300

    TODO AddNoteTest

--- app/src/test/java/com/rodrigoc/noteapp/feature_note/domain/use_case/AddNoteTest.kt
+++ app/src/test/java/com/rodrigoc/noteapp/feature_note/domain/use_case/AddNoteTest.kt
@@ -1,5 +1,25 @@
 package com.rodrigoc.noteapp.feature_note.domain.use_case
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth.assertThat
+import com.rodrigoc.noteapp.feature_note.domain.model.InvalidNoteException
+import com.rodrigoc.noteapp.feature_note.domain.model.Note
+import com.rodrigoc.noteapp.feature_note.domain.repository.FakeNotesRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Assert.assertThrows
+import org.junit.Before
+import org.junit.Test
+import kotlin.jvm.Throws
 
-class AddNoteTest
+//TODO:
+class AddNoteTest {
+
+    private lateinit var note: Note
+    private lateinit var addNote: AddNote
+    private lateinit var repository: FakeNotesRepository
+
+    @Before
+    fun setUp() {
+        repository = FakeNotesRepository()
+    }
+}
